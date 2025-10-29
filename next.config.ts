ca7b41ab12168850c6eb8680859a5dc4aef80cbe
sheppardjm <jsheppard@neucadia.com>
2025-10-29 11:14:53 -0400

    Next setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import { dirname } from "node:path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: dirname(__filename),
+  },
 };
 
 export default nextConfig;
