a13dc9d2b5bcb7d456ce4e547ff53f79f3e3b5dc
sheppardjm <jsheppard@neucadia.com>
2025-10-30 10:42:31 -0400

    Add caching

--- src/lib/data/articles.ts
+++ src/lib/data/articles.ts
@@ -1,10 +1,26 @@
 import { eq } from "drizzle-orm";
 import { usersSync } from "drizzle-orm/neon";
-import db from "@/db";
+import redis from "@/cache";
+import db from "@/db/index";
 import { articles } from "@/db/schema";
 
-export async function getArticles() {
-  const res = await db
+export type ArticleList = {
+  id: number;
+  title: string;
+  createdAt: string;
+  content: string;
+  author: string | null;
+  imageUrl?: string | null;
+};
+
+export async function getArticles(): Promise<ArticleList[]> {
+  const cached = await redis.get<ArticleList[]>("articles:all");
+  if (cached) {
+    console.log("🎯 Get Articles Cache Hit!");
+    return cached;
+  }
+
+  const response = await db
     .select({
       title: articles.title,
       id: articles.id,
@@ -14,11 +30,29 @@ export async function getArticles() {
     })
     .from(articles)
     .leftJoin(usersSync, eq(articles.authorId, usersSync.id));
-  return res;
+
+  console.log("🏹 Get Articles Cache Miss!");
+  try {
+    await redis.set("articles:all", JSON.stringify(response), {
+      ex: 60,
+    });
+  } catch (err) {
+    console.warn("Failed to set articles cache", err);
+  }
+  return response as unknown as ArticleList[];
 }
 
+export type ArticleWithAuthor = {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  imageUrl?: string | null;
+  author: string | null;
+};
+
 export async function getArticleById(id: number) {
-  const res = await db
+  const response = await db
     .select({
       title: articles.title,
       id: articles.id,
@@ -30,5 +64,5 @@ export async function getArticleById(id: number) {
     .from(articles)
     .where(eq(articles.id, id))
     .leftJoin(usersSync, eq(articles.authorId, usersSync.id));
-  return res[0] ? res[0] : null;
+  return response[0] ? (response[0] as unknown as ArticleWithAuthor) : null;
 }
