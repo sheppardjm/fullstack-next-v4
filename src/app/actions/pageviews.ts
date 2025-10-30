8f6279a29f967e255010869ed3304ac43ec8e252
sheppardjm <jsheppard@neucadia.com>
2025-10-30 11:33:54 -0400

    Add email

--- src/app/actions/pageviews.ts
+++ src/app/actions/pageviews.ts
@@ -1,11 +1,19 @@
 "use server";
 
 import redis from "@/cache";
+import sendCelebrationEmail from "@/email/celebration-email";
+
+const milestones = [20, 100, 500, 1000, 5000, 10000];
 
 const keyFor = (id: number | string) => `pageviews:article:${id}`;
 
 export async function incrementPageview(articleId: number) {
   const articleKey = keyFor(articleId);
   const newVal = await redis.incr(articleKey);
-  return +newVal;
+
+  if (milestones.includes(newVal)) {
+    sendCelebrationEmail(articleId, newVal);
+  }
+
+  return newVal;
 }
---
+++ src/email/celebration-email.ts
@@ -0,0 +1,58 @@
+import { eq } from "drizzle-orm";
+import { usersSync } from "drizzle-orm/neon";
+import resend from "@/email";
+import db from "@/db";
+import { articles } from "@/db/schema";
+import CelebrationTemplate from "@/email/templates/celebration-template";
+
+const BASE_URL = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export default async function sendCelebrationEmail(
+  articleId: number,
+  pageviews: number
+) {
+  const res = await db
+    .select({
+      email: usersSync.email,
+      id: usersSync.id,
+      title: articles.title,
+      name: usersSync.name,
+    })
+    .from(articles)
+    .leftJoin(usersSync, eq(articles.authorId, usersSync.id))
+    .where(eq(articles.id, articleId));
+
+  const { email, id, name, title } = res[0];
+
+  if (!email) {
+    throw new Error("Author email not found");
+  }
+
+  /* If custom domain is setup
+    const emailRes = await resend.emails.send({
+    from: "Wikimasters",
+    to: email,
+    subject: "Wikimasters Article Pageviews",
+    html: `<h1>Congratulations!</h1><p>Your article has received ${pageviews} views.</p>`,
+  }); */
+
+  const emailRes = await resend.emails.send({
+    from: "Wikimasters <onboarding@resend.dev>",
+    to: "jamisonsheppard@gmail.com",
+    subject: "Wikimasters Article Pageviews",
+    react: CelebrationTemplate({
+      name: name || undefined,
+      articleTitle: title || undefined,
+      articleUrl: `${BASE_URL}/wiki/${articleId}`,
+      pageviews,
+    }),
+  });
+
+  if (!emailRes.error) {
+    console.log(
+      `Celebration email sent to user ID ${id} for article ID ${articleId}`
+    );
+  }
+}
